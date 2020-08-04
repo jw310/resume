b0133e2c35c3565be8b400a8aec3e515ca6cd4d7
Jw <jerrywu1003@gmail.com>
2020-08-04 19:40:29 +0800

    update skills

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
-($(document).ready(function () {  // 等所有的 HTML 都載入，且 DOM 都以建構完成執行 { } 內容，將所有 function() 放入裡面是常見做法
+// 等所有的 HTML 都載入，且 DOM 都以建構完成執行 { } 內容，將所有 function() 放入裡面是常見做法
+($(document).ready(function () {
   //console.log("Hello World!");
   $('.slick').slick({
     dots: true,             //顯示輪播圖片會顯示圓圈
